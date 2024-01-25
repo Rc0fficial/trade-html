10734a5b44b0c1623145a801ae1e7ef83517715f
rc0fficial <to.arsalanali@gmail.com>
2024-01-25 15:50:37 +0500

    main tabs working

--- script.js
+++ script.js
@@ -1,33 +1,35 @@
-function activateTab(index) {
-    const tabs = document.querySelectorAll(".tab");
+function activateMainTab(selectedTab, contentId) {
+    // Remove 'active' class from all tabs
+    document.querySelectorAll(".main-tab").forEach(tab => tab.classList.remove("active"));
 
-    tabs.forEach((tab, i) => {
-      const liElement = tab.closest("li");
-      const tabText = tab.querySelector(".tab-text");
-
-      if (liElement && tabText) {
-        liElement.classList.toggle("bg-[#E5FFF5]", i === index);
-        liElement.style.borderLeft =
-          i === index ? "2px solid #13B440" : "none";
-        tabText.classList.toggle("text-[#13B440]", i === index);
-      }
-    });
-  }
-
-  function activateTabItem(selectedTab, contentId) {
-    // Remove 'active-tab' class from all tabs
-    document
-      .querySelectorAll(".tab-item")
-      .forEach((tab) => tab.classList.remove("active-tab"));
-
-    // Add 'active-tab' class to the selected tab
-    selectedTab.classList.add("active-tab");
+    // Add 'active' class to the selected tab
+    selectedTab.classList.add("active");
 
     // Hide all tab content
-    document
-      .querySelectorAll(".tab-content")
-      .forEach((content) => content.classList.remove("active"));
+    document.querySelectorAll(".main-tab-content").forEach(content => content.classList.remove("active"));
 
     // Show the corresponding tab content
     document.getElementById(contentId).classList.add("active");
-  }
+}
+
+
+
+
+
+function activateTabItem(selectedTab, contentId) {
+  // Remove 'active-tab' class from all tabs
+  document
+    .querySelectorAll(".tab-item")
+    .forEach((tab) => tab.classList.remove("active-tab"));
+
+  // Add 'active-tab' class to the selected tab
+  selectedTab.classList.add("active-tab");
+
+  // Hide all tab content
+  document
+    .querySelectorAll(".tab-content")
+    .forEach((content) => content.classList.remove("active"));
+
+  // Show the corresponding tab content
+  document.getElementById(contentId).classList.add("active");
+}
